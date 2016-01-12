db941ed0a4ec639ec78b963554930c14a4fb901c
Aman Garg <amangargcse@outlook.com>
2016-01-12 15:15:20 +0530

    Add : Day 11

--- HASKELL/day10.hs
+++ HASKELL/day10.hs
@@ -1,10 +1,8 @@
 -- Day 10 = Binary
 
-main ::IO()
-main = interact $ unlines . map solve . drop 1 . lines
-
-solve :: String -> String
-solve = concatMap show . convertToBin . read
 
 convertToBin :: Int -> [Int]
 convertToBin = reverse . map (`mod` 2) . takeWhile (>0) . iterate (`div` 2)
+
+main :: IO()
+main = interact $ unlines . map (concatMap show . convertToBin . read ) . drop 1 . lines
